2620419893714bc921fd168cdbef6906105fcc1c
kate <kdj4027@gmail.com>
2023-03-22 17:17:20 +0900

    JS: Home Transparent

--- main.js
+++ main.js
@@ -37,6 +37,14 @@ contactBtn.addEventListener("click", (event) => {
   // scrollTo.scrollIntoView({ behavior: "smooth" });
 });
 
+// Make home slowly foade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+  // console.log(1 - window.scrollY / homeHeight);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
